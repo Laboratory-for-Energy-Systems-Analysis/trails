a5afdb0163019ea870c6ad209820dabde85ce54c
romainsacchi <r_s@me.com>
2026-01-29 21:15:02 +0100

    add directed graph

--- trails/__init__.py
+++ trails/__init__.py
@@ -8,6 +8,7 @@ __all__ = (
     "get_lcia_method_names",
     "plot_temporal_scores",
     "clear_cache",
+    "search_activity",
 )
 
 __version__ = "1.0.0"
@@ -17,3 +18,4 @@ from .lca import lca
 from .lcia import get_lcia_method_names
 from .plotting import plot_temporal_scores
 from .cache import clear_cache
+from .search import search_activity
